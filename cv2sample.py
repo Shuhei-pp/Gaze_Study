c82b847f365531a580ac4766e96373aa1cf67523
Shuhei-pp <lotakotayota@gmail.com>
2021-12-06 21:29:09 +0900

    pcのビデオをキャプチャー

--- cv2sample.py
+++ cv2sample.py
@@ -1,33 +1,52 @@
 # coding: utf-8 
+import sys
 import cv2
 
 import matplotlib.pyplot as plt
 
+# videocaptureのインスタンス作成
+cap = cv2.VideoCapture(0)
+
+# カメラ使用できない場合
+if cap.isOpened() is False:
+    print("カメラが使用できません")
+    sys.exit()
+
+
 # 顔分類器読込
 cascade_path = "haarcascade_frontalface_default.xml"
-
-img = cv2.imread('sample.jpg')
-
-# グレースケール変換
-img_gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # カスケード検出器の特徴量抽出
 cascade = cv2.CascadeClassifier(cascade_path)
 
-# 顔検出実行
-facerect = cascade.detectMultiScale(img_gry, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+while True:
+    # videocaptureで1フレーム読み込む
+    ret, frame = cap.read()
 
-# 顔検出の色
-rectangle_color = (0, 255, 0)#緑
+    # グレースケール変換
+    frame_gry = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-if len(facerect) > 0:
-    for rect in facerect:
-        cv2.rectangle(img, tuple(rect[0:2]),tuple(rect[0:2] + rect[2:4]), rectangle_color,thickness=2)
+    # 顔検出実行
+    facerect = cascade.detectMultiScale(frame_gry, scaleFactor=1.1, minNeighbors=2,  minSize=(30, 30))
 
-cv2.namedWindow("display", cv2.WINDOW_NORMAL)
+    # 顔検出の色
+    rectangle_color = (0, 255, 0)#緑
 
-# ウインドウ表示
-cv2.imshow("display",img)
-cv2.waitKey(0)
+    # 枠線描写
+    if len(facerect) > 0:
+        for rect in facerect:
+            cv2.rectangle(frame, tuple(rect[0:2]),tuple(rect[0:2] + rect[2:4]), rectangle_color,thickness=2)
+
+    cv2.namedWindow("display", cv2.WINDOW_NORMAL)
+
+    # ウインドウ表示
+    cv2.imshow("display", frame)
+
+    # ESCで終了
+    k = cv2.waitKey(1)
+    if k == 27: #ESC
+        break
+
 cv2.destroyAllWindows()
- 
+
+
